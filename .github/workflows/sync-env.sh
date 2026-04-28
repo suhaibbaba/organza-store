5028731293641e286fa709251ecc2f71e18dbe5c
suhaibbaba <suhaib.m.baba@gmail.com>
2026-04-28 21:46:06 +0300

    Update workflow

---
+++ .github/workflows/sync-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ln -sf /root/organza-store-env/.env-backend  /root/organza-store/backend/.env
+ln -sf /root/organza-store-env/.env-frontend /root/organza-store/frontend/.env
+ln -sf /root/organza-store-env/.env-pos      /root/organza-store/pos/.env
